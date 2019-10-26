e4edd072ea05cbc419f2e5abd58e90f7b8320c7e
Carl Wirth <cdwirth1@gmail.com>
2019-10-26 17:03:27 -0400

    ?? I changed something..

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const prompt = require("readline-sync");
 const wordBank = require("./word-bank.json");
 
 // this is where you are building your project. 
-prompt.question("Pick a letter.");
+
+console.log("\n Welcome to Hangman!\nPress ctrl+c to stop.\n");
+
+const letter = prompt.question("Pick a letter: ");
