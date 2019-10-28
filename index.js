ff07411fda973ba16eb8f3c9897850b1a8ad23a6
Carl Wirth <cdwirth1@gmail.com>
2019-10-27 21:24:09 -0400

    created blank lines

--- index.js
+++ index.js
@@ -21,10 +21,23 @@ let myRandomElement = wordBank.randomElement();
 
 console.log("'" + myRandomElement + "' is the magic word. (I'll hide it later.)\n ");
 
-console.log("'" + myRandomElement + "' has " + myRandomElement.length + " letters in it.");
+console.log("'" + myRandomElement + "' has " + myRandomElement.length + " letters in it. \n");
 
+
+for (let i = 0; i < (myRandomElement.length); i++) {
+    console.log(" _ ");
+};
+
+const letter = prompt.question(userName + ", please guess a letter: ");
 
 
 // for (let i = letterCount; i > 0; i--) {
 //     console.log(userName + ', you have ' + i + 'guesses left.')
 // };
+
+console.log("\n " + userName + ", would you like to play again?");
+if (readlineSync.keyInYNStrict("   Hit Y or N. ")) {
+    console.log("\nWelcome back, " + userName + ".\n   (Press 'ctrl+c' to stop.)\n");
+} else {
+    console.log("\n...sadness.\n   I'll miss you " + userName + '!' + "\n        Goodbye.")
+};
